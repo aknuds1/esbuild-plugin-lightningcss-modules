2aa26f664cc3dcd9a82756b789f5422ebc6adf09
Arve Knudsen <arve.knudsen@gmail.com>
2023-12-31 13:23:34 +0100

    Camel case ES exports
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import {bundle,} from 'lightningcss';
 import {createHash,} from 'crypto';
 import fs from 'fs/promises';
 import {dirname, join,} from 'path';
+import camelCase from 'camelcase';
 
 /**
  * A generic cssModules plugin for esbuild based on lightningcss
@@ -19,120 +20,120 @@ import {dirname, join,} from 'path';
  */
 export const cssModules = (options = {}) => {
     return {
-        name: 'css-modules',
-        setup: ({onLoad, onResolve, initialOptions}) => {
-            const transpiledCssModulesMap = new Map()
+      name: 'css-modules',
+      setup: ({onLoad, onResolve, initialOptions}) => {
+        const transpiledCssModulesMap = new Map()
 
-            onResolve({filter: /^css-modules:\/\//}, ({path}) => {
-                return {
-                    namespace: 'css-modules',
-                    path,
-                }
-            })
+        onResolve({filter: /^css-modules:\/\//}, ({path}) => {
+          return {
+            namespace: 'css-modules',
+            path,
+          }
+        })
 
-            onLoad({filter: /.*/, namespace: 'css-modules'}, ({path}) => {
-                const {code, resolveDir} = transpiledCssModulesMap.get(path)
-                return {
-                    contents: code,
-                    loader: initialOptions.loader?.['.css'] ?? 'css',
-                    resolveDir,
+        onLoad({filter: /.*/, namespace: 'css-modules'}, ({path}) => {
+          const {code, resolveDir} = transpiledCssModulesMap.get(path)
+          return {
+            contents: code,
+            loader: initialOptions.loader?.['.css'] ?? 'css',
+            resolveDir,
+          }
+        })
 
-                }
-            })
+        onLoad({filter: options.includeFilter ?? /\.module\.css$/}, async ({path}) => {
+          if (options.excludeFilter?.test(path)) {
+              return;
+          }
 
-            onLoad({filter: options.includeFilter ?? /\.module\.css$/}, async ({path}) => {
-                if (options.excludeFilter?.test(path)) {
-                    return;
-                }
+          const cssModules = { pattern: options.cssModulesPattern ?? `[hash]_[local]`, ...options.cssModules }
 
-                const cssModules = { pattern: options.cssModulesPattern ?? `[hash]_[local]`, ...options.cssModules }
+          const { code, map, exports } = bundle({
+              filename: path,
+              analyzeDependencies: false,
+              cssModules,
+              sourceMap: true,
+              targets: options.targets,
+              drafts: options.drafts,
+              visitor: options.visitor,
+              customAtRules: options.customAtRules,
+              // this way the correct relative path for the source map will be generated ;)
+              projectRoot: join(initialOptions.absWorkingDir || process.cwd(), initialOptions.outdir)
+          });
+          if (!exports) {
+              return;
+          }
 
-                const { code, map, exports } = bundle({
-                    filename: path,
-                    analyzeDependencies: false,
-                    cssModules,
-                    sourceMap: true,
-                    targets: options.targets,
-                    drafts: options.drafts,
-                    visitor: options.visitor,
-                    customAtRules: options.customAtRules,
-                    // this way the correct relative path for the source map will be generated ;)
-                    projectRoot: join(initialOptions.absWorkingDir || process.cwd(), initialOptions.outdir)
-                });
-                if (!exports) {
-                    return;
-                }
+          const id = 'css-modules:\/\/' + createHash('sha256').update(path).digest('base64url') + '.css'
 
-                const id = 'css-modules:\/\/' + createHash('sha256').update(path).digest('base64url') + '.css'
+          const finalCode = code.toString('utf8') + `/*# sourceMappingURL=data:application/json;base64,${map.toString('base64')} */`;
 
-                const finalCode = code.toString('utf8') + `/*# sourceMappingURL=data:application/json;base64,${map.toString('base64')} */`;
+          transpiledCssModulesMap.set(
+            id,
+            { code: finalCode, resolveDir: dirname(path) }
+          )
 
-                transpiledCssModulesMap.set(
-                    id,
-                    { code: finalCode, resolveDir: dirname(path) }
-                )
+          const quote = JSON.stringify;
 
-                const quote = JSON.stringify;
+          const escape = (string) => JSON.stringify(string).slice(1, -1)
 
-                const escape = (string) => JSON.stringify(string).slice(1, -1)
+          let contents = '';
 
-                let contents = '';
+          /** @type {Map<string, string>} */
+          const dependencies = new Map()
 
-                /** @type {Map<string, string>} */
-                const dependencies = new Map()
+          /** @param {String} path */
+          const importDependency = (path) => {
+            if (dependencies.has(path)) {
+              return dependencies.get(path)
+            }
+            const dependenciesName = `dependency_${dependencies.size}`
+            // prepend dependeny to to the contents
+            contents = `import ${dependenciesName} from ${quote(path)}\n` + contents;
+            dependencies.set(path, dependenciesName)
+            return dependenciesName;
+          }
 
-                /** @param {String} path */
-                const importDependency = (path) => {
-                    if (dependencies.has(path)) {
-                        return dependencies.get(path)
-                    }
-                    const dependenciesName = `dependency_${dependencies.size}`
-                    // prepend dependeny to to the contents
-                    contents = `import ${dependenciesName} from ${quote(path)}\n` + contents;
-                    dependencies.set(path, dependenciesName)
-                    return dependenciesName;
-                }
+          contents += `import ${quote(id)}\n`;
+          contents += `export default {`;
 
-                contents += `import ${quote(id)}\n`;
-                contents += `export default {`;
+          for (const [cssClassReadableName, cssClassExport] of Object.entries(exports)) {
+              let compiledCssClasses = `'${escape(cssClassExport.name)}`
 
-                for (const [cssClassReadableName, cssClassExport] of Object.entries(exports)) {
-                    let compiledCssClasses = `'${escape(cssClassExport.name)}`
+              if (cssClassExport.composes) {
+                  for (const composition of cssClassExport.composes) {
+                      switch (composition.type) {
+                        case 'local':
+                          compiledCssClasses += ' ' + escape(composition.name)
+                          break;
+                    
+                        case 'global':
+                          compiledCssClasses += ' ' + escape(composition.name)
+                          break;
 
-                    if (cssClassExport.composes) {
-                        for (const composition of cssClassExport.composes) {
-                            switch (composition.type) {
-                                case 'local':
-                                    compiledCssClasses += ' ' + escape(composition.name)
-                                    break;
-                            
-                                case 'global':
-                                    compiledCssClasses += ' ' + escape(composition.name)
-                                    break;
+                        case 'dependency':
+                          compiledCssClasses += ` ' + ${importDependency(composition.specifier)}[${quote(composition.name)}] + '`
+                          break;
+                      }
+                  }
+              }
 
-                                case 'dependency':
-                                    compiledCssClasses += ` ' + ${importDependency(composition.specifier)}[${quote(composition.name)}] + '`
-                                    break;
-                            }
-                        }
-                    }
+              compiledCssClasses += `'`
 
-                    compiledCssClasses += `'`
+              const jsName = camelCase(cssClassReadableName);
+              contents += `${quote(jsName)}:${compiledCssClasses},`
+            }
 
-                    contents += `${quote(cssClassReadableName)}:${compiledCssClasses},`
-                }
+            contents += '}'
 
-                contents += '}'
+            // https://github.com/evanw/esbuild/issues/2943#issuecomment-1439755408
+            const emptyishSourceMap = 'data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==';
+            contents += `\n//# sourceMappingURL=${emptyishSourceMap}`
 
-                // https://github.com/evanw/esbuild/issues/2943#issuecomment-1439755408
-                const emptyishSourceMap = 'data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIiJdLCJtYXBwaW5ncyI6IkEifQ==';
-                contents += `\n//# sourceMappingURL=${emptyishSourceMap}`
-
-                return {
-                    contents,
-                    loader: 'js',
-                }
-            })
-        }    
-    }
+            return {
+              contents,
+              loader: 'js',
+            }
+        })
+    }    
+  }
 };
